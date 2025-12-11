d07252a501bef3653a854638bda00b0639e9893a
Felipe00lk <felipecruzfc185@gmail.com>
2025-12-11 02:56:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/* ===================== JAVASCRIPT (script.js) ===================== */
-function scrollToSection(id) {
-  const section = document.getElementById(id);
-  section.scrollIntoView({ behavior: "smooth" });
-}
-const dark=document.querySelector("#dark");
-dark.onclick= () =>{
-  document.body.classList.toggle("escuro");
-};
